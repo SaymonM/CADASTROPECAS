43b4ed9fb05e0da95af5c61c2b7f3fa0ff93c7e1
SaymonM <saymonmeura@hotmail.com>
2021-08-19 20:47:17 -0400

    Atividade Encontro Remoto 1

---
+++ pecas.js
@@ -0,0 +1,27 @@
+const ListaDePecas = ["Mola", "Calota", "Freio", "Para-choque"]
+
+console.log(ListaDePecas)
+
+if(ListaDePecas.length < 10){
+    console.log("É possível cadastrar mais peças")
+} else {
+    console.log("Não há mais espaço na lista")
+}
+
+var peso = 79;
+
+if (peso > 100){
+    console.log("Peso da peça está adequado")
+} else {
+    console.log("Peso insuficiente")
+}
+
+let nomePeca = "Disco de Freio";
+
+if (nomePeca.length > 3){
+    console.log("Nome adequado")
+} else if (nomePeca.length == 0){
+    console.log("O nome não pode ser vazio")
+} else {
+    console.log("O nome deve possuir mais de 3 caracteres")
+}
